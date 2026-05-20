6656f909fa7c4e33eaa8f950ae848b45b949f4a4
Eduardo-Unicamp <e260057@dac.unicamp.br>
2026-05-20 15:06:40 -0300

    implementa json parser e mudanças correlatas no menu e na classe CepRequestHandler

--- src/Main.java
+++ src/Main.java
@@ -16,33 +16,82 @@ public class Main {
         String userInput = "";
 
         while (!userInput.equals("0")){
-            System.out.println("\nDigite o CEP que deseja buscar ou 0 para sair: ");
+            System.out.println("\nSelecione a opção desejada: \n" +
+                    "0 - sair\n" +
+                    "1 - buscar cep\n" +
+                    "2 - buscar logradouro, bairro e localidade de um cep\n");
             userInput =s.nextLine();
 
-            if(!userInput.equals("0")){
-                try {
-                    System.out.println(" ... Buscando informações... ");
-                    HttpResponse<String> response = requestHandler.getCEP(userInput);
+            switch (userInput){
+                case "0"://stop application
+                    break;
+                case "1"://look for all info for that CEP
+                    System.out.println("\nDigite o CEP que deseja buscar ou 0 para sair: ");
 
-                    System.out.printf("Sucesso na requisição.\n" +
-                            "---------------------\n" +
-                            " Status:%s\n" +
-                            "---------------------" +
-                                    "%s",
-                            response.statusCode(),response.body());
+                    userInput = s.nextLine();
+                    if(!userInput.equals("0")){
+                        try {
+                            System.out.println(" ... Buscando informações... ");
+                            HttpResponse<String> response = requestHandler.getCEP(userInput);
+
+                            System.out.printf("Sucesso na requisição.\n" +
+                                            "---------------------\n" +
+                                            " Status:%s\n" +
+                                            "---------------------" +
+                                            "%s",
+                                    response.statusCode(),response.body());
 
 
-                }catch(CepInvalidoException e) {
-                    System.out.println(e.getMessage());
-                }catch (RequisicaoMalSucedidaException e){
-                    System.out.println(e.getMessage());
-                }catch(CepInexistenteException e){
-                    System.out.println(e.getMessage());
-                }
-            }else{
-                System.out.println("Encerrando aplicação...");
-                break;
+                        }catch(CepInvalidoException e) {
+                            System.out.println(e.getMessage());
+                        }catch (RequisicaoMalSucedidaException e){
+                            System.out.println(e.getMessage());
+                        }catch(CepInexistenteException e){
+                            System.out.println(e.getMessage());
+                        }
+                    }else{
+                        System.out.println("Encerrando aplicação...");
+                        break;
+                    }
+
+                    break;
+                case "2"://look for only Address info(logradouro, bairro e localidade)
+                    System.out.println("\nDigite o CEP que deseja buscar ou 0 para sair: ");
+                    userInput = s.nextLine();
+                    if(!userInput.equals("0")){
+                        try {
+                            System.out.println(" ... Buscando informações... ");
+                            Address address = requestHandler.getAddress(userInput);
+
+                            System.out.printf("Sucesso na requisição.\n" +
+                                            "---------------------\n" +
+                                            "Logradouro: %s \n" +
+                                            "Bairro: %s\n" +
+                                            "Localidade: %s\n",
+                                            address.getLogradouro(),
+                                            address.getBairro(),
+                                            address.getLocalidade() );
+
+
+                        }catch(CepInvalidoException e) {
+                            System.out.println(e.getMessage());
+                        }catch (RequisicaoMalSucedidaException e){
+                            System.out.println(e.getMessage());
+                        }catch(CepInexistenteException e){
+                            System.out.println(e.getMessage());
+                        }
+                    }else{
+                        System.out.println("Encerrando aplicação...");
+                        break;
+                    }
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor selecione uma opção válida!");
+
             }
+
+
+
 
             }
         }
